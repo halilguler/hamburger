648d6da2434319ff3a85044f8b080e650d8de7fe
halilguler <halilguler1994@gmail.com>
2020-09-09 00:15:24 +0300

    step01: Creating layout Component step02: Burger Building container step03: Adding Prop Types Validation step04: Burger Component step05: Dynamical Burger Ingredients step06: Calculating the Ingredient Sum Dynamically

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Layout>
+                    <BurgerBuilder/>
+                </Layout>
+                <h1>Hamburger</h1>
+            </div>
+        );
+    }
 
-function App() {
-  return (
-    <div className="App">
-<h1>Hamburger</h1>
-    </div>
-  );
 }
 
 export default App;
