5b6d1d4a05fb4d24ef4bb5f9e1d4e91d2a0ec031
halilguler <halilguler1994@gmail.com>
2021-01-05 20:40:31 +0300

    step01: Creating layout Component step02: Burger Building container step03: Adding Prop Types Validation step04: Burger Component step05: Dynamical Burger Ingredients step06: Calculating the Ingredient Sum Dynamically

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,23 @@
 import React from "react";
 
-const buildControls = (props) =>{
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.css';
+
+const controls = [
+    {label: 'Salad', type: "salad"},
+    {label: 'Bacon', type: "bacon"},
+    {label: 'Cheese', type: "cheese"},
+    {label: 'Meat', type: "meat"},
+]
+
+const buildControls = (props) => {
     return (
-        <div></div>
+        <div className={classes.BuildControls}>
+            {controls.map(ctrl => (
+                <BuildControl key={ctrl.label} label={ctrl.label} remove={() => props.removeIngredients(ctrl.type)}
+                              added={() => props.addedIngredients(ctrl.type)} disabled={props.disabled[ctrl.type]}/>
+            ))}
+        </div>
     );
 }
 
