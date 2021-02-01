c6067d78c57095987c655a44a8f7884118182368
halil.guler <halil.guler@zanhagrup.com>
2021-02-01 14:15:19 +0300

    step01: add testing NavigationItems step02: edit setProps testing step03: edit contains property testing

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -6,11 +6,25 @@ import Adapter from "enzyme-adapter-react-16";
 import NavigationItems from "./NavigationItems";
 import NavigationItem from "./NavigationItem/NavigationItem";
 
-configure({ adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 describe("<NavigationItems />", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
   it("should render two <NavigationItem /> elements if not authentication", () => {
-    const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it("should render three <NavigationItem /> elements if authentication", () => {
+    wrapper.setProps({ isAuthentication: true });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
+  it("should render three <NavigationItem link={'/logout'}>Logout</NavigationItem> elements if authentication", () => {
+    wrapper.setProps({ isAuthentication: true });
+    expect(wrapper.contains(<NavigationItem link={"/logout"}>Logout</NavigationItem>)).toEqual(true);
   });
 });
