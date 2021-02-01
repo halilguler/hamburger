f1417d9620e8ddb69438b34fcf4c375f6433723b
halil.guler <halil.guler@zanhagrup.com>
2021-02-01 10:29:01 +0300

    step01: add Suspense step02: add React.lazy asyncComponent

--- src/App.js
+++ src/App.js
@@ -1,62 +1,66 @@
-import React, {Component} from 'react';
-import {Route, Switch, Redirect, withRouter} from 'react-router-dom';
-import * as actions from './store/actions/index';
+import React, { Component, Suspense } from "react";
+import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+import * as actions from "./store/actions/index";
 import Layout from "./hoc/Layout/Layout";
-import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from "./containers/Auth/Auth";
-import Logout from "./containers/Auth/Logout/Logout";
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
+import Spinner from "./components/UI/Spinner/Spinner";
+
+const burgerBuilder = React.lazy(() =>
+  import("./containers/BurgerBuilder/BurgerBuilder")
+);
+const logout = React.lazy(() => import("./containers/Auth/Logout/Logout"));
+const orders = React.lazy(() => import("./containers/Orders/Orders"));
+const auth = React.lazy(() => import("./containers/Auth/Auth"));
+const checkout = React.lazy(() => import("./containers/Checkout/Checkout"));
 
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
 
-    componentDidMount() {
-        this.props.onTryAutoSignup();
+  render() {
+    let router = (
+      <Switch>
+        <Route path={"/auth"} component={auth} />
+        <Route path={"/"} exact component={burgerBuilder} />
+        <Redirect to={"/"} />
+      </Switch>
+    );
+    if (this.props.isAuthentication) {
+      router = (
+        <Switch>
+          <Route path={"/checkout"} component={checkout} />
+          <Route path={"/orders"} component={orders} />
+          <Route path={"/auth"} component={auth} />
+          <Route path={"/logout"} component={logout} />
+          <Route path={"/"} exact component={burgerBuilder} />
+          <Redirect to={"/"} />
+          {/*<Route render={() => (<h2>404 not found!</h2>)}/>*/}
+          {/*<Redirect from={'/checkout'} to={'/'}/>*/}
+        </Switch>
+      );
     }
 
-    render() {
-
-        let router = (
-            <Switch>
-                <Route path={"/auth"} component={Auth}/>
-                <Route path={"/"} exact component={BurgerBuilder}/>
-                <Redirect to={'/'}/>
-            </Switch>
-        )
-        if (this.props.isAuthentication) {
-            router = (<Switch>
-                <Route path={"/checkout"} component={Checkout}/>
-                <Route path={"/orders"} component={Orders}/>
-                <Route path={"/auth"} component={Auth}/>
-                <Route path={"/logout"} component={Logout}/>
-                <Route path={"/"} exact component={BurgerBuilder}/>
-                <Redirect to={'/'}/>
-                {/*<Route render={() => (<h2>404 not found!</h2>)}/>*/}
-                {/*<Redirect from={'/checkout'} to={'/'}/>*/}
-            </Switch>)
-        }
-
-        return (
-            <div className="App">
-                <Layout>
-                    {router}
-                </Layout>
-            </div>
-        );
-    }
+    return (
+      <div className="App">
+        <Suspense fallback={<Spinner />}>
+          <Layout>{router}</Layout>
+        </Suspense>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        isAuthentication: state.auth.token !== null,
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    isAuthentication: state.auth.token !== null,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onTryAutoSignup: () => dispatch(actions.authCheckState())
-    }
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState()),
+  };
+};
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
