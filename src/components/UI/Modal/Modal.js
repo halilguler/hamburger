eb11bacc7d8c4506074eeb0f9f15e7a0ae62a410
halil.guler <halil.guler@zanhagrup.com>
2021-02-10 10:57:14 +0300

    step01: edit class component to hooks component changed

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,32 @@
-import React, {Component} from 'react';
-import classes from './Modal.css';
-import Aux from '../../../hoc/Auxiliary/Auxiliary';
-import Backdrop from '../Backdrop/Backdrop';
+import React, { Component } from "react";
+import classes from "./Modal.css";
+import Aux from "../../../hoc/Auxiliary/Auxiliary";
+import Backdrop from "../Backdrop/Backdrop";
 
-class Modal extends Component {
+const Modal = (props) => {
+  // shouldComponentUpdate(nextProps, nextState, nextContext) {
+  //     return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
+  // }
 
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-    }
+  return (
+    <Aux>
+      <Backdrop show={this.props.show} clicked={this.props.clicked} />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: this.props.show ? "1" : "0",
+        }}
+      >
+        {this.props.children}
+      </div>
+    </Aux>
+  );
+};
 
-    render() {
-        return (
-            <Aux>
-                <Backdrop show={this.props.show} clicked={this.props.clicked}/>
-                <div className={classes.Modal}
-                     style={{
-                         transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                         opacity: this.props.show ? '1' : '0'
-                     }}>{this.props.children}</div>
-            </Aux>);
-    }
-}
-
-export default Modal;
+export default React.memo(
+  Modal,
+  (prevProps, nextProps) =>
+    nextProps.show === prevProps.show &&
+    nextProps.children === prevProps.children
+);
